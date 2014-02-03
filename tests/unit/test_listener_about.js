61d4d7692e858a97025bd4436785caf8d4650cea
Fernando Rodriguez Sela <frsela@tid.es>
2014-02-03 15:59:05 +0100

    Updated credits and file headers

--- tests/unit/test_listener_about.js
+++ tests/unit/test_listener_about.js
@@ -1,9 +1,10 @@
 /* jshint node: true */
 /**
  * Wake Up Platform
- * (c) Telefonica Digital, 2013 - All rights reserved
+ * (c) Telefonica Digital, 2014 - All rights reserved
  * License: GNU Affero V3 (see LICENSE file)
- * Fernando Rodríguez Sela <frsela@tid.es>
+ * Fernando Rodríguez Sela <frsela at tid dot es>
+ * Guillermo López Leal <gll at tid dot es>
  */
 
 var request = require('request'),
